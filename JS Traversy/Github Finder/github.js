a3fb5b0b35c840f1e1d936ec58cc69d0f7efea3e
Max Fleischman <maxecf@gmail.com>
2019-01-30 15:39:31 -0800

    Add files via upload

---
+++ JS Traversy/Github Finder/github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+        this.client_id ='f097e3c010cbb118f3c3';
+        this.client_secret='cb3fade3528cd287f849aea6c4d0179f1c0f24a6';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
